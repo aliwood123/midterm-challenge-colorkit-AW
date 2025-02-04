c3e11d3e0ec104c796ad7bef90ab87438e0d1801
Alicia Woodard <awoodard1@ewu.edu>
2025-02-04 18:25:42 +0000

    Add js, css, and connect js library

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 // website loaded?
-console.log(Okay);
+// ✅ Add External ColorKit Library
+// Color Kit Library
+// https://codepen.io/manikoth/pen/NPKVOxm.js
+
+// Get Stuff
+const body = document.querySelector("body");
+
+// Set variable for HTML `body` element
+
+// ✅ Check-Check: log out the `body` element in the console
+console.log(body);
+
+// ✅ Check-Check: get and attach the node for the background color with `.style.backgroundColor`
+console.log(body.style.backgroundColor);
+// Set Stuff
+
+// ✅ Check-Check: assign a background color to "hard code it"
+// Hint: Is the color a string, number, or boolean?
+body.style.backgroundColor = "pink";
+
+// dynamically set backgound color with `colorkit.random()` function/method
+body.style.backgroundColor = colorkit.random();
+// Sparkle effect for funzies
+// replace❓ with variable for the `body` element
+// colorkit.sparkle( ❓);
+colorkit.sparkle(body);
